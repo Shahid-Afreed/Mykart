7521a61bd229ad90fe8301cd37399b602b9af336
Shahid-Afreed <shahidafreed643@gmail.com>
2024-10-28 11:37:51 +0530

    first commit

--- products/views.py
+++ products/views.py
@@ -14,16 +14,40 @@ def index(request):
 
 
 def list_products(request):
-    page=1
-    if request.GET:
-        page=request.GET.get('page',1)
-    product_list=Product.objects.order_by('priority')
+    sort_option = request.GET.get('sort', 'default')   
+    # Get the current page, default to 1
+    page = request.GET.get('page', 1)
+    # Retrieve the product list
+    product_list = Product.objects.all()  # Get all products
+    # Apply sorting based on the selected option
+    if sort_option == 'Price low to high':
+        product_list = product_list.order_by('price')
+    elif sort_option== 'Price high to low':
+        product_list = product_list.order_by('-price') 
+    else:
+        product_list = product_list.order_by('priority')  
 
-    product_paginator=Paginator(product_list,2)
-    product_list=product_paginator.get_page(page)
+    # Paginate the sorted product list
+    product_paginator = Paginator(product_list, 6)
+    product_list = product_paginator.get_page(page)
 
-    context={'products':product_list}
-    return render(request,'products.html',context)
+    context = {
+        'products': product_list,
+        'sort_option': sort_option,  # Pass the sort option to the template if needed
+    }
+    return render(request, 'products.html', context)
+
+    #PREVIOUS CODE
+    # page=1
+    # if request.GET:
+    #     page=request.GET.get('page',1)
+    # product_list=Product.objects.order_by('priority')
+
+    # product_paginator=Paginator(product_list,6)
+    # product_list=product_paginator.get_page(page)
+
+    # context={'products':product_list}
+    # return render(request,'products.html',context)
 
 
 
